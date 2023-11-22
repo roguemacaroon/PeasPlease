445ed3880faa0abfdefe23984df1de07e9187ec9
Trish <roguemacaroon@gmail.com>
2023-11-21 23:43:09 -0400

    added some css and changed the pop up

--- PeasPlease/script.js
+++ PeasPlease/script.js
@@ -2,6 +2,6 @@ console.log(1 + 1)
 
 // When the user clicks on <div>, open the popup
 function myFunction() {
-    var popup = document.getElementById("myPopup");
+    var popup = document.getElementById("MyPopup");
     popup.classList.toggle("show");
 }
